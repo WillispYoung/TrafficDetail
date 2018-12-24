ddbbb6f208946e841471fb9a76be2e97f6b756d5
willispyoung <willispyoung@gmail.com>
2018-12-24 09:40:05 +0800

    integrate with data

--- src/traffic-detail.js
+++ src/traffic-detail.js
@@ -14,6 +14,7 @@ var width, height, lblockW, rblockW;
 const svg = d3.select("#everything");
 var title = svg.append("image").attr("xlink:href", "src/title.png");
 var map = svg.append("image").attr("xlink:href", "src/map-cut-2.png");
+var statistics = svg.append("text").attr("id", "statistics");
 
 width = $(window).width() * 0.97;
 height = $(window).height() * 0.96;
@@ -36,6 +37,8 @@ imageX = lblockW + (rblockW - imageW) / 2;
 imageY = (height - imageH) / 2;
 map.attr("x", imageX).attr("y", imageY).attr("width", imageW);
 
+statistics.attr("x", lblockW / 2).attr("y", height * 0.88);
+
 const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
 const transition = svg.transition()
     .duration(500)
@@ -52,6 +55,35 @@ function gradient(fraction, type) {
     return ans;
 }
 
+function updateStatistics(stat, date) {
+    statistics.selectAll("tspan").remove();
+
+    time = "";
+    for (var i in date) {
+        if (i == 0)
+            time += date[i].toString() + "年";
+        else if (i == 1)
+            time += date[i].toString() + "月";
+        else if (i == 2)
+            time += "第" + date[i].toString() + "周";
+        else
+            time += date[i].toString() + "日";
+    }
+    period = stat.Time.split(":");
+
+    statistics.append("tspan")
+        .text(time + "联网总时长为" +
+            period[0].toString() + "小时" +
+            period[1].toString() + "分钟" +
+            period[2].toString() + "秒，");
+    statistics.append("tspan")
+        .text("其中入校流量为" + stat.in +
+            "，出校流量为" + stat.out + "。");
+    statistics.selectAll("tspan")
+        .attr("x", lblockW / 2)
+        .attr("dy", "1.2em");
+}
+
 async function drawTree() {
     function getNode(root, date) {
         //date = [year, month, week, day]'s any prefix
@@ -61,17 +93,20 @@ async function drawTree() {
             node = node["" + date[0]];
         }
         for (var i = 1; i < date.length; i++) {
+            if (typeof(node) == "undefined")
+                return nullNode;
+
             node = node["children"]["" + date[i]];
         }
-        //node may be undefined
+        //node may be undefined: non-existing date
         if (typeof(node) == "undefined") {
-            return nullNode
+            return nullNode;
         } else {
             return node;
         }
     }
 
-    tree = await d3.json("data/myTree.json");
+    tree = await d3.json("data/tree-reformatted.json");
     status = "y"; // "ym", "ymw", "ymwd"
     selected_item = undefined;
 
@@ -120,6 +155,10 @@ async function drawTree() {
                 id = d3.select(this).attr("id");
                 index = parseInt(tree[id].id);
 
+                if (index == center.year) {
+                    updateStatistics(tree[id].stat, [parseInt(id)]);
+                }
+
                 yearX = lblockW / 3;
                 yearY = (height - metric.year[center.year]) / 2;
                 if (index < center.year)
@@ -156,6 +195,11 @@ async function drawTree() {
                 id = d3.select(this).attr("id");
                 index = parseInt(id.split("-")[1]) - 1;
 
+                if (index == center.month) {
+                    root = getNode(tree, id.split("-"));
+                    updateStatistics(root.stat, [originYear, index + 1]);
+                }
+
                 monthX = lblockW / 3;
                 monthY = (height - metric.month[center.month]) / 2;
                 if (index < center.month)
@@ -190,10 +234,10 @@ async function drawTree() {
             .attr("transform", `translate(${-lblockW*0.15},${height*0.2}) scale(0.6)`);
 
         status = "ym";
+        center.month = 6;
         computeMonthWidth();
 
         originYear = selected_item.attr("id");
-        root = tree[originYear];
         for (var i = 0; i < 12; i++) {
             index = i;
             month = svg.append("g")
@@ -226,14 +270,17 @@ async function drawTree() {
                     else {
                         status = "ym";
                         selected_item = d3.select(this);
-                        center.month = parseInt(id.split("-")[1]);
+                        center.month = parseInt(selected_item.attr("id").split("-")[1]) - 1;
                         collapseCalendar();
                         repositionMonthes();
                     }
                 });
 
-            if (index == center.month)
+            if (index == center.month) {
                 selected_item = month;
+                root = getNode(tree, [originYear, index + 1]);
+                updateStatistics(root.stat, [originYear, index + 1]);
+            }
 
             monthX = lblockW / 3;
             monthY = (height - metric.month[center.month]) / 2;
@@ -274,12 +321,26 @@ async function drawTree() {
 
         status = "ymw";
 
+        previousID = selected_item.attr("id");
+        date = previousID.split("-").map(x => parseInt(x));
+
+        // Monday: 0, Tuesday: 1, ...
+        firstDay = (new Date(date[0], date[1] - 1, 1).getDay() + 7) % 8;
+        daysInMonth = new Date(date[0], date[1], 0).getDate();
+        weeksInMonth = Math.floor((firstDay + daysInMonth - 1) / 7) + 1;
+
+        weeks = [
+            []
+        ];
+        for (var i = 0; i < weeksInMonth - 1; i++) weeks.push([]);
+        for (var i = 0; i < daysInMonth; i++) {
+            weekIndex = Math.floor((firstDay + i) / 7);
+            weeks[weekIndex].push(i + 1);
+        }
+
         unit = lblockW / (3 * 8);
-        originYearMonth = selected_item.attr("id").split("-");
-        root = getNode(tree, originYearMonth);
-        for (var key in root.children) {
-            weekId = selected_item.attr("id") + "-" + key;
-            indexW = parseInt(key);
+        for (var i in weeks) {
+            weekId = previousID + "-" + (parseInt(i) + 1).toString();
             week = svg.append("g")
                 .attr("class", "week")
                 .attr("id", weekId)
@@ -287,76 +348,126 @@ async function drawTree() {
                     d3.select(this).attr("cursor", "pointer");
                 })
                 .on("click", function() {
-                    if (status == "ymw" && weekId != selected_item.attr("id")) {
+                    weekId = d3.select(this).attr("id");
+                    if (status == "ymw" && selected_item.attr("id") != weekId) {
+                        selected_item.select(".week-circle")
+                            .attr("stroke", "#566b8c")
+                            .attr("stroke-width", 3);
+
                         selected_item = d3.select(this);
+                        selected_item.select(".week-circle")
+                            .attr("stroke", "#639eff")
+                            .attr("stroke-width", 4);
+
+                        root = getNode(tree, weekId.split("-"));
+                        updateStatistics(root.stat, weekId.split("-"));
+                    } else if (status == "ymwd") {
+                        selected_item.select(".day-rect")
+                            .attr("stroke", null)
+                            .attr("stroke-width", null);
+
+                        status = "ymw";
+                        selected_item = d3.select(this);
+                        selected_item.select(".week-circle")
+                            .attr("stroke", "#639eff")
+                            .attr("stroke-width", 4);
+
+                        root = getNode(tree, weekId.split("-"));
+                        updateStatistics(root.stat, weekId.split("-"));
                     }
                 });
 
-            if (indexW == 1)
+            dayY = height / 2 + (i - weeks.length / 2) * (unit + 5) - 2.5;
+            week.append("circle")
+                .attr("class", "week-circle")
+                .attr("r", unit * 0.25)
+                .attr("cx", lblockW / 2)
+                .attr("cy", dayY + unit / 2)
+                .attr("fill", "#d3f0ff")
+                .attr("stroke", "#566b8c")
+                .attr("stroke-width", 3);
+
+            if (i == 0) {
                 selected_item = week;
+                selected_item.select(".week-circle")
+                    .attr("stroke", "#639eff")
+                    .attr("stroke-width", 4);
 
-            subroot = root.children[key];
-            firstDay = undefined;
-            weekLength = Object.keys(subroot).length;
-            for (var subkey in subroot.children) {
-                if (firstDay == undefined)
-                    firstDay = parseInt(subkey);
-                dayId = weekId + "-" + subkey;
-                dayX = indexW == 1 ?
-                    lblockW / 2 + ((parseInt(subkey) - firstDay) + 7 - weekLength) * (unit + 5) :
-                    lblockW / 2 + (parseInt(subkey) - firstDay) * (unit + 5);
-                dayY = height / 2 + (indexW - 6 / 2 - 1) * (unit + 5) - 2.5;
+                root = getNode(tree, weekId.split("-"));
+                updateStatistics(root.stat, weekId.split("-"));
+            }
 
-                week.append("rect")
+            for (var j in weeks[i]) {
+                dayX = i == 0 ?
+                    lblockW / 2 + (parseInt(j) + 7 - weeks[i].length) * (unit + 5) :
+                    lblockW / 2 + j * (unit + 5);
+
+                dayX += unit * 0.8;
+
+                day = svg.append("g")
                     .attr("class", "day")
-                    .attr("id", dayId)
+                    .attr("id", weekId + "-" + weeks[i][j].toString())
+                    .on("mouseover", function() {
+                        d3.select(this).attr("cursor", "pointer");
+                    })
+                    .on("click", function() {
+                        dayId = d3.select(this).attr("id");
+                        if (status == "ymw") {
+                            // change from week to day
+                            selected_item.select(".week-circle")
+                                .attr("stroke", "#566b8c")
+                                .attr("stroke-width", 3);
+
+                            status = "ymwd";
+                            selected_item = d3.select(this);
+                            selected_item.select(".day-rect")
+                            	.attr("stroke", "#ff5151")
+                                .attr("stroke-width", 3);
+
+                            date = dayId.split("-");
+                            updateStatistics(getNode(tree, date).stat, date);
+                        } else if (status == "ymwd" && dayId != selected_item.attr("id")) {
+                            // change day
+                            selected_item.select(".day-rect")
+                            	.attr("stroke", null)
+                                .attr("stroke-width", null);
+
+                            selected_item = d3.select(this);
+                            selected_item.select(".day-rect")
+                                .attr("stroke", "#ff5151")
+                                .attr("stroke-width", 3);
+
+                            date = dayId.split("-");
+                            updateStatistics(getNode(tree, date).stat, date);
+                        }
+                    });
+
+                day.append("rect")
+                    .attr("class", "day-rect")
                     .attr("x", dayX)
                     .attr("y", dayY)
                     .attr("width", unit)
                     .attr("height", unit)
-                    .attr("fill", gradient((indexW - 1) / 7, 4))
-                    .on("mouseover", function() {
-                        d3.select(this).attr("cursor", "pointer");
-                    })
-                    .on("mouseout", function() {
+                    .attr("fill", gradient(i / weeks.length, 4));
 
-                    })
-                    .on("click", function() {
-                        id_day = d3.select(this).attr("id");
-                        if (status == "ymw") {
-                            // select day in a week
-                            status = "ymwd";
-                            selected_element = d3.select(this);
-                            d3.select(this)
-                                .attr("stroke", "#ff5151")
-                                .attr("stroke-width", 3);
-                        } else if (status == "ymwd" && id_day == selected_element.attr("id")) {
-                            // focus on this day
-
-                        } else {
-                            // change selected day
-                            selected_element.attr("stroke", null)
-                                .attr("stroke-width", null);
-                            selected_element = d3.select(this);
-                            d3.select(this)
-                                .attr("stroke", "#ff5151")
-                                .attr("stroke-width", 3);
-                        }
-                    });
-
-                week.append("text")
+                day.append("text")
                     .attr("class", "day-text")
                     .attr("x", dayX)
                     .attr("y", dayY)
                     .attr("dx", unit / 2)
                     .attr("dy", unit / 2)
-                    .text(subkey);
+                    .text(weeks[i][j]);
             }
         }
     }
 
     function collapseAll() {
         d3.selectAll(".week")
+            .transition(transition)
+            .attr("opacity", "0.3")
+            .attr("transform", `translate(${lblockW/3},0)`)
+            .remove();
+        d3.selectAll(".day")
             .transition(transition)
             .attr("opacity", "0.3")
             .attr("transform", `translate(${lblockW/3},0)`)
@@ -373,6 +484,11 @@ async function drawTree() {
 
     function collapseCalendar() {
         d3.selectAll(".week")
+            .transition(transition)
+            .attr("opacity", "0.3")
+            .attr("transform", `translate(${lblockW/3},0)`)
+            .remove();
+        d3.selectAll(".day")
             .transition(transition)
             .attr("opacity", "0.3")
             .attr("transform", `translate(${lblockW/3},0)`)
@@ -394,7 +510,7 @@ async function drawTree() {
                 d3.select(this).attr("cursor", "pointer");
                 d3.select(this).select(".year-rect")
                     .attr("stroke", "pink")
-                    .attr("stroke-width", 2);
+                    .attr("stroke-width", 4);
             })
             .on("mouseout", function() {
                 d3.select(this).select(".year-rect")
@@ -429,13 +545,9 @@ async function drawTree() {
             yearY += metric.year[0] + 5;
         yearW = lblockW / 3 - 10 * index;
 
-        if (index == 0) {
+        if (index == center.year) {
             selected_item = year;
-
-            svg.append("text")
-            	.attr("class", "year-stat")
-            	.attr("x", lblockW / 3)
-            	.attr("y", height * 0.8);
+            updateStatistics(tree[key].stat, [parseInt(key)]);
         }
 
         year.append("rect")
