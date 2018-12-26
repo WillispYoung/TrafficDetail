6fe87d4abeecd9c0d24dc387a181e3e95f67cd78
willispyoung <willispyoung@gmail.com>
2018-12-26 21:49:53 +0800

    nearly accomplished

--- src/traffic-detail.js
+++ src/traffic-detail.js
@@ -10,6 +10,8 @@ const colorSet = [
 ];
 
 var width, height, lblockW, rblockW;
+var ipTextX = -30,
+    ipTextY = -80;
 
 const svg = d3.select("#everything");
 var title = svg.append("image").attr("xlink:href", "src/title.png");
@@ -20,6 +22,36 @@ width = $(window).width() * 0.97;
 height = $(window).height() * 0.96;
 lblockW = width * 8 / 13;
 rblockW = width * 5 / 13;
+
+var nodeColor = "white",
+    nodeHoverColor = "red";
+var nodeR = 5,
+    nodeHoverR = 10;
+var rectBorder = "black";
+
+var chart = svg.append("g").attr("class", "chart")
+    .attr("transform", "translate(50," + height * 0.7 + ")");
+
+function getNode(root, date) {
+    //date = [year, month, week, day]'s any prefix
+    node = root;
+    nullNode = { "stat": { "out": "0B", "Time": "00:00:00", "in": "0B" }, "block": [] };
+    if (date.length >= 1) {
+        node = node["" + date[0]];
+    }
+    for (var i = 1; i < date.length; i++) {
+        if (typeof(node) == "undefined")
+            return nullNode;
+
+        node = node["children"]["" + date[i]];
+    }
+    //node may be undefined: non-existing date
+    if (typeof(node) == "undefined") {
+        return nullNode;
+    } else {
+        return node;
+    }
+}
 
 svg.attr("width", width).attr("height", height);
 
@@ -39,7 +71,7 @@ map.attr("x", imageX).attr("y", imageY).attr("width", imageW);
 
 statistics.attr("x", lblockW / 2).attr("y", height * 0.88);
 
-const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
+//const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
 const transition = svg.transition()
     .duration(500)
     .attr("height", height)
@@ -58,55 +90,501 @@ function gradient(fraction, type) {
 function updateStatistics(stat, date) {
     statistics.selectAll("tspan").remove();
 
-    time = "";
-    for (var i in date) {
-        if (i == 0)
-            time += date[i].toString() + "年";
-        else if (i == 1)
-            time += date[i].toString() + "月";
-        else if (i == 2)
-            time += "第" + date[i].toString() + "周";
-        else
-            time += date[i].toString() + "日";
-    }
-    period = stat.Time.split(":");
+    if (status != "ymwd") {
+        time = "";
+        for (var i in date) {
+            if (i == 0)
+                time += date[i].toString() + "年";
+            else if (i == 1)
+                time += date[i].toString() + "月";
+            else if (i == 2)
+                time += "第" + date[i].toString() + "周";
+            else
+                time += date[i].toString() + "日";
+        }
+        period = stat.Time.split(":");
 
-    statistics.append("tspan")
-        .text(time + "联网总时长为" +
-            period[0].toString() + "小时" +
-            period[1].toString() + "分钟" +
-            period[2].toString() + "秒，");
-    statistics.append("tspan")
-        .text("其中入校流量为" + stat.in +
-            "，出校流量为" + stat.out + "。");
-    statistics.selectAll("tspan")
-        .attr("x", lblockW / 2)
-        .attr("dy", "1.2em");
+        statistics.append("tspan")
+            .text(time + "联网总时长为" +
+                period[0].toString() + "小时" +
+                period[1].toString() + "分钟" +
+                period[2].toString() + "秒，");
+        statistics.append("tspan")
+            .text("其中入校流量为" + stat.in +
+                "，出校流量为" + stat.out + "。");
+        statistics.selectAll("tspan")
+            .attr("x", lblockW / 2)
+            .attr("dy", "1.2em");
+    }
+
+    d3.selectAll(".ipText").remove();
+    d3.selectAll("defs").remove();
+    d3.selectAll("marker").remove();
+    d3.selectAll(".route").remove();
+    d3.selectAll(".link").remove();
+    d3.selectAll(".node").remove();
+    d3.selectAll(".gantt-chart").remove();
+    d3.selectAll(".xAxis").remove();
+    d3.selectAll(".yAxis").remove();
+    drawEdge();
+    if (status == "ymwd")
+        drawChart();
 }
 
-async function drawTree() {
-    function getNode(root, date) {
-        //date = [year, month, week, day]'s any prefix
-        node = root;
-        nullNode = { "stat": { "out": "0B", "Time": "00:00:00", "in": "0B" }, "block": [] };
-        if (date.length >= 1) {
-            node = node["" + date[0]];
-        }
-        for (var i = 1; i < date.length; i++) {
-            if (typeof(node) == "undefined")
-                return nullNode;
+// ========================================================================//
+function getCurrentFocus() {
+    if (status == "y" || status == "ym")
+        return [lblockW * 0.67, height / 2];
+    else if (status == "ymw")
+        return [lblockW * 0.9, parseFloat(selected_item.select(".week-circle").attr("cy"))];
+    else
+        return [lblockW * 0.9, lblockW / 48 + parseFloat(selected_item.select(".day-rect").attr("y"))];
+}
 
-            node = node["children"]["" + date[i]];
-        }
-        //node may be undefined: non-existing date
-        if (typeof(node) == "undefined") {
-            return nullNode;
-        } else {
-            return node;
-        }
+function getIpPosition(ip) {
+    return [imageX + ip2pos[ip].x / pixelW * imageW, imageY + ip2pos[ip].y / pixelH * imageH];
+}
+
+function flowStr2Float(flow) {
+    var type = flow[flow.length - 1];
+
+    num = parseFloat(flow.slice(0, flow.length - 1));
+    if (type == 'B')
+        return num;
+    else if (type == 'K')
+        return num * 1024;
+    else if (type == 'M')
+        return num * 1024 ** 2;
+    else if (type == 'G')
+        return num * 1024 ** 3;
+
+    return num;
+}
+
+function curvePoint(a, b) {
+    var shift = 20;
+    var mid = [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2];
+    var verti = [b[1] - a[1], a[0] - b[0]];
+    var lenVer = Math.sqrt(verti[0] ** 2 + verti[1] ** 2);
+    verti = [verti[0] / lenVer * shift, verti[1] / lenVer * shift];
+    return [mid[0] + verti[0], mid[1] + verti[1]];
+}
+
+function drawEdge() {
+    var curNode = getNode(tree, selected_item.attr("id").split("-"));
+    if (curNode["block"].length == 0)
+        return;
+
+    var edgeTree = {}
+    edgeTree["name"] = "";
+    edgeTree["children"] = curNode["block"];
+
+    var d3Tree = d3.hierarchy(edgeTree);
+    var curFocus = getCurrentFocus();
+    d3Tree["x"] = curFocus[0];
+    d3Tree["y"] = curFocus[1];
+    var edges = [];
+    var routes = [];
+
+    var allIn = 0;
+    d3Tree["children"].forEach(function(d) {
+        allIn += flowStr2Float(d.data["in"]);
+    });
+
+    var lastPos = [];
+    d3Tree["children"].forEach(function(d) {
+        var curPos = getIpPosition(d.data["ip"]);
+        d["x"] = curPos[0];
+        d["y"] = curPos[1];
+        cenX = (curFocus[0] + curPos[0]) / 2;
+        cenY = (curFocus[1] + curPos[1]) / 2;
+        edges.push({
+            "p": [curFocus,
+                [(cenX + curFocus[0]) / 2, curFocus[1]],
+                [cenX, cenY],
+                [(cenX + curPos[0]) / 2, curPos[1]],
+                curPos
+            ],
+            "frac": (1 - Math.log10(1 + 9 * flowStr2Float(d.data["in"]) / allIn))
+        });
+        if (lastPos.length > 0 && lastPos.toString() != curPos.toString())
+            routes.push([lastPos,
+                curvePoint(lastPos, curPos),
+                curPos
+            ]);
+        lastPos = curPos;
+    });
+
+    if (status == "ymwd") {
+        var line = d3.line()
+            .x(function(d) { return d[0]; })
+            .y(function(d) { return d[1]; })
+            .curve(d3.curveCardinal.tension(0.5));
+
+        var defs = svg.append("defs");
+
+        var arrowMarker = defs.append("marker")
+            .attr("id", "arrow")
+            .attr("markerUnits", "strokeWidth")
+            .attr("markerWidth", "6")
+            .attr("markerHeight", "6")
+            .attr("viewBox", "0 0 6 6")
+            .attr("refX", "3")
+            .attr("refY", "3")
+            .attr("orient", "auto");
+
+        var arrow_path = "M1,1 L5,3 L1,5 L3,3 L1,1";
+
+        arrowMarker.append("path")
+            .attr("d", arrow_path)
+            .attr("fill", "#0e2e3d");
+
+        var route = svg.append("g").selectAll(".route")
+            .data(routes)
+            .enter().append("path")
+            .attr("class", "route")
+            .attr("d", line)
+            .attr("stroke", "red")
+            //.attr("marker-start", "url(#arrow)")
+            .attr("marker-mid", "url(#arrow)")
+        //.attr("marker-end", "url(#arrow)");
     }
+    var link = svg.append("g").selectAll(".link"),
+        node = svg.append("g").selectAll(".node");
+
+    link = link
+        .data(edges)
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", function(d) {
+            var e = d["p"]
+            return `M${e[0][0]} ${e[0][1]} 
+        S${e[1][0]} ${e[1][1]} ${e[2][0]} ${e[2][1]} 
+        S${e[3][0]} ${e[3][1]} ${e[4][0]} ${e[4][1]}`;
+        })
+        .attr('stroke', function(d) {
+            return gradient(d["frac"], 4);
+        });
+
+    ipText = d3.select("body")
+        .append("div")
+        .attr("class", "ipText")
+        .style("opacity", 0.0);
+
+    node = node
+        .data(d3Tree.leaves())
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("fill", nodeColor)
+        .attr("r", nodeR)
+        .on("mouseover", function(d) {
+            d3.select(this)
+                .attr("cursor", "pointer")
+                .attr("fill", nodeHoverColor)
+                .attr("r", nodeHoverR);
+
+            ipText.html(d.data.ip + "<br>" + d.data.Time + "<br>" + d.data.in)
+                .style("left", (ipTextX + d3.event.pageX) + "px")
+                .style("top", (ipTextY + d3.event.pageY) + "px")
+                .style("opacity", 1.0);
+
+            chart.selectAll(".gantt-chart").selectAll("rect")
+                .attr("stroke", function(dd) {
+                    if (dd.ip == d.data.ip)
+                        return rectBorder;
+                    else
+                        return null;
+                })
+                .attr("stroke-width", function(dd) {
+                    if (dd.ip == d.data.ip)
+                        return 2;
+                    else
+                        return null;
+                });
+        }).on("mousemove", function(d) {
+            ipText.style("left", (ipTextX + d3.event.pageX) + "px")
+                .style("top", (ipTextY + d3.event.pageY) + "px");
+        }).on("mouseout", function(d) {
+            ipText.style("opacity", 0.0)
+                .style("left", "0px")
+                .style("top", "0px");
+            d3.select(this)
+                .attr("fill", nodeColor)
+                .attr("r", nodeR);
+            chart.selectAll(".gantt-chart").selectAll("rect")
+                .attr("stroke", null)
+                .attr("stroke-width", null);
+        });
+}
+
+// ========================================================================//
+
+
+// ========================================================================//
+d3.gantt = function() {
+    var FIT_TIME_DOMAIN_MODE = "fit";
+    var FIXED_TIME_DOMAIN_MODE = "fixed";
+
+    var margin = {
+        top: 20,
+        right: 40,
+        bottom: 20,
+        left: 50
+    };
+    var selector = 'body';
+    var timeDomainStart = d3.timeDay.offset(new Date(), -3);
+    var timeDomainEnd = d3.timeHour.offset(new Date(), +3);
+    var timeDomainMode = FIT_TIME_DOMAIN_MODE; // fixed or fit
+    var taskTypes = [];
+    var taskStatus = [];
+    var chartHeight = height * 0.3; //document.body.clientHeight - margin.top - margin.bottom-5;
+    var chartWidth = lblockW * 0.9; //document.body.clientWidth - margin.right - margin.left-5;
+
+    var linePad = chartHeight / 4;
+    var lineHeight = linePad;
+
+    var tickFormat = "%H:%M";
+
+    var keyFunction = function(d) {
+        return d ? d.startDate + d.taskName + d.endDate : "";
+    };
+
+    var rectTransform = function(d) {
+        return "translate(" + xd(d.startDate) + "," +
+            (parseFloat(d["#device"]) * linePad + lineHeight * (0.5 - 0.5 * d.hRatio) - linePad).toString() + ")";
+    };
+
+    var initTimeDomain = function(tasks) {
+        if (timeDomainMode === FIT_TIME_DOMAIN_MODE) {
+            if (tasks === undefined || tasks.length < 1) {
+                timeDomainStart = d3.timeDay.offset(new Date(), -3);
+                timeDomainEnd = d3.timeHour.offset(new Date(), +3);
+                return;
+            }
+            tasks.sort(function(a, b) {
+                return a.endDate - b.endDate;
+            });
+            timeDomainEnd = tasks[tasks.length - 1].endDate;
+            tasks.sort(function(a, b) {
+                return a.startDate - b.startDate;
+            });
+            timeDomainStart = tasks[0].startDate;
+        }
+    };
+    var xd, yd, xAxis, yAxis;
+    var initAxis = function() {
+        xd = d3.scaleTime().domain([timeDomainStart, timeDomainEnd]).range([0, chartWidth]).clamp(true);
+
+        yd = d3.scaleBand().domain(taskTypes).range([0, chartHeight - margin.top - margin.bottom]).padding(0.1);
+
+        xAxis = d3.axisBottom().scale(xd).ticks(7).tickFormat(d3.timeFormat(tickFormat))
+            .tickSize(8).tickPadding(8);
+
+        yAxis = d3.axisLeft().scale(yd).tickSize(0);
+    };
+    initAxis();
+
+    function gantt(tasks) {
+        initTimeDomain(tasks);
+        initAxis();
+
+        chart.append("g")
+            .attr("class", "xAxis")
+            .attr("transform", "translate(0, " + (chartHeight - margin.top - margin.bottom) + ")")
+            .transition()
+            .call(xAxis);
+
+        chart.append("g").attr("class", "yAxis").transition().call(yAxis);
+
+        chart
+            .append("g").attr("class", "gantt-chart")
+            .selectAll(".gantt-chart")
+            .data(tasks, keyFunction)
+            .enter()
+            .append("rect")
+            .attr("rx", 5)
+            .attr("ry", 5)
+            .attr("class", function(d) {
+                if (taskStatus[d.status] == null)
+                    return "bar";
+                return taskStatus[d.status];
+            })
+            //.attr("y", 0)
+            //.attr("title", "test")
+            //.append("title").text(function(d) { return "test"; })
+            //.on("mouseover",function(d,i){
+            //  d3.select(this)
+            //      .attr("fill","yellow");
+            //})
+            .attr("transform", rectTransform)
+            .attr("height", function(d) {
+                return lineHeight * d.hRatio;
+            })
+            .attr("width", function(d) {
+                return Math.max(1, (xd(d.endDate) - xd(d.startDate)));
+            })
+
+            .on("mouseover", function(d) {
+                d3.select(this)
+                    .attr("cursor", "pointer")
+                    .attr("stroke", rectBorder)
+                    .attr("stroke-width", 2);
+
+                ipText.html(d.status + "<br>" + d.inflow +
+                        "<br>" + d.begin + "-" + d.end)
+                    .style("left", (ipTextX + d3.event.pageX) + "px")
+                    .style("top", (ipTextY + d3.event.pageY) + "px")
+                    .style("opacity", 1.0);
+
+                d3.selectAll(".node")
+                    .attr("fill", function(dd) {
+                        if (dd.data.ip == d.ip)
+                            return nodeHoverColor;
+                        else
+                            return nodeColor;
+                    })
+                    .attr("r", function(dd) {
+                        if (dd.data.ip == d.ip)
+                            return nodeHoverR;
+                        else
+                            return nodeR;
+                    });
+            }).on("mousemove", function(d) {
+                ipText.style("left", (ipTextX + d3.event.pageX) + "px")
+                    .style("top", (ipTextY + d3.event.pageY) + "px");
+            }).on("mouseout", function(d) {
+                d3.select(this)
+                    .attr("stroke", null)
+                    .attr("stroke-width", null);
+
+                ipText.style("opacity", 0.0)
+                    .style("left", "0px")
+                    .style("top", "0px");
+
+                d3.selectAll(".node")
+                    .attr("fill", nodeColor)
+                    .attr("r", nodeR);
+            });
+
+        return gantt;
+    };
+
+    gantt.margin = function(value) {
+        if (!arguments.length)
+            return margin;
+        margin = value;
+        return gantt;
+    };
+
+    gantt.timeDomain = function(value) {
+        if (!arguments.length)
+            return [timeDomainStart, timeDomainEnd];
+        timeDomainStart = +value[0], timeDomainEnd = +value[1];
+        return gantt;
+    };
+
+    gantt.timeDomainMode = function(value) {
+        if (!arguments.length)
+            return timeDomainMode;
+        timeDomainMode = value;
+        return gantt;
+
+    };
+
+    gantt.taskTypes = function(value) {
+        if (!arguments.length)
+            return taskTypes;
+        taskTypes = value;
+        return gantt;
+    };
+
+    gantt.taskStatus = function(value) {
+        if (!arguments.length)
+            return taskStatus;
+        taskStatus = value;
+        return gantt;
+    };
+
+    gantt.width = function(value) {
+        if (!arguments.length)
+            return chartWidth;
+        chartWidth = +value;
+        return gantt;
+    };
+
+    gantt.height = function(value) {
+        if (!arguments.length)
+            return chartHeight;
+        chartHeight = +value;
+        return gantt;
+    };
+
+    gantt.tickFormat = function(value) {
+        if (!arguments.length)
+            return tickFormat;
+        tickFormat = value;
+        return gantt;
+    };
+
+    gantt.selector = function(value) {
+        if (!arguments.length)
+            return selector;
+        selector = value;
+        return gantt;
+    };
+
+    return gantt;
+};
+
+function drawChart() {
+    var curNode = getNode(tree, selected_item.attr("id").split("-"));
+    if (curNode["block"].length == 0)
+        return;
+
+    var maxIn = 1;
+    flowStr2Float(curNode["stat"]["in"]);
+    curNode["block"].forEach(function(d) {
+        maxIn = Math.max(maxIn, flowStr2Float(d["in"]))
+    });
+    var tasks = [];
+    curNode["block"].forEach(function(d) {
+        tasks.push({
+            "startDate": new Date("Sun Dec 09 " + d["begin"] + " GMT+0800 (中国标准时间)"),
+            "endDate": new Date("Sun Dec 09 " + d["end"] + " GMT+0800 (中国标准时间)"),
+            "#device": d["device#"],
+            "status": d["device"],
+            "ip": d["ip"],
+            "inflow": d["in"],
+            "begin": d["begin"],
+            "end": d["end"],
+            "hRatio": flowStr2Float(d["in"]) / maxIn + 0.05
+        });
+    });
+
+    var taskStatus = {
+        "Windows": "bar",
+        "Smartphones/PDAs/Tablets": "bar-failed",
+        "Macintosh": "bar-running",
+        "???": "bar-killed"
+    };
+
+    var taskNames = ["1", "2", "3"];
+
+    var format = "%H:%M";
+
+    var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+
+    gantt(tasks);
+
+}
+// ========================================================================//
+
+async function drawTree() {
 
     tree = await d3.json("data/tree-reformatted.json");
+    ip2pos = await d3.json("data/ip2pos.json");
     status = "y"; // "ym", "ymw", "ymwd"
     selected_item = undefined;
 
@@ -325,7 +803,7 @@ async function drawTree() {
         date = previousID.split("-").map(x => parseInt(x));
 
         // Monday: 0, Tuesday: 1, ...
-        firstDay = (new Date(date[0], date[1] - 1, 1).getDay() + 7) % 8;
+        firstDay = (new Date(date[0], date[1] - 1, 1).getDay() + 6) % 7;
         daysInMonth = new Date(date[0], date[1], 0).getDate();
         weeksInMonth = Math.floor((firstDay + daysInMonth - 1) / 7) + 1;
 
